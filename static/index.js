6a51d92a5b9a1470550d854370e1b743c3874961
Saurabh Kumar Singh <77530794+sksusha8853@users.noreply.github.com>
2022-01-02 00:49:37 +0530

    dark mode added

---
+++ static/index.js
@@ -0,0 +1,12 @@
+var var1=0;
+function changeMode(){
+  if(var1==0){
+    var1=1;
+    document.body.style.backgroundColor="black";
+  }
+  else{
+    var1=0;
+    document.body.style.backgroundColor="white";
+  }
+
+}
